dee56c8fae2632c8c958d19f768945d6cee00fb7
emmanuel-jan <50714005+emmanuel-jan@users.noreply.github.com>
2020-05-09 19:51:23 +0300

    Update db.php

--- Admin/include/db.php
+++ Admin/include/db.php
@@ -1,4 +1,4 @@
 <?php
 //establishing a connection to the database
-$conn=mysqli_connect("sql309.epizy.com","epiz_25651421","KsRMDPtnKMuOJSZ","epiz_25651421_westlabels"); 
+$conn=mysqli_connect("us-cdbr-east-06.cleardb.net","b956bb0ee3087a","bc710314","heroku_6ea7b0f1369f42f""); 
 ?>
